0f792dff4bfb722e0c094a2600271c7fce7fd7a1
irodadavletova073-rgb <irodadavletova073@gmail.com>
2026-05-29 13:48:21 +0500

    Add: 3) Menyu: hafta kuni [irodadavletova073-rgb]

---
+++ lms_kod_topshiriqlari/Shartlar_bilan_menyu_tuzish__oddiy_CLI_menyu_/3__Menyu__hafta_kuni.py
@@ -0,0 +1,13 @@
+day = input()
+if day == "1":
+    print("Mon")
+elif day == "2":
+    print("Tue")
+elif day == "3":
+    print("Wed")
+elif day == "4":
+    print("Thu")
+elif day == "5":
+    print("Fri")
+else:
+    print("Other")
