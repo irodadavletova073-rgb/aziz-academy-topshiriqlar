3696e04cce561916216079ae4f0735766bb2974d
irodadavletova073-rgb <irodadavletova073@gmail.com>
2026-05-29 22:09:49 +0500

    Add: 15) 1..n ichida 3 ta juft son topilgach to‘xtash [irodadavletova073-rgb]

---
+++ lms_kod_topshiriqlari/while_bilan_break_va_continue/15__1__n_ichida_3_ta_juft_son_topilgach_to_xtash.py
@@ -0,0 +1,10 @@
+n = int(input())
+count = 0
+for i in range(1, n+1):
+    if i % 2 == 0:
+        count += 1
+        if count == 3:
+            print(i)
+            break
+else:
+            print("No")
